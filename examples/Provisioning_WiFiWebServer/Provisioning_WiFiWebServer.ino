c3140d91bd072095c325d87eb51bfeb2c2751367
Sandeep Mistry <s.mistry@arduino.cc>
2016-09-09 16:32:40 -0400

    Initial provisioning mode improvements
    - Add new example sketch
    - add new status types for provisioning
    - Use m2m_wifi_get_connection_info to get SSID when connected with
    default connect

---
+++ examples/Provisioning_WiFiWebServer/Provisioning_WiFiWebServer.ino
@@ -0,0 +1,157 @@
+/*
+  WiFi Web Server
+
+ A simple web server that shows the value of the analog input pins.
+ using a WiFi shield.
+
+ This example is written to configure the WiFi settings using provisioning mode.
+
+ Circuit:
+ * WiFi shield attached
+ * Analog inputs attached to pins A0 through A5 (optional)
+
+ created 13 July 2010
+ by dlf (Metodo2 srl)
+ modified 31 May 2012
+ by Tom Igoe
+
+ */
+
+#include <SPI.h>
+#include <WiFi101.h>
+
+int status = WL_IDLE_STATUS;
+
+WiFiServer server(80);
+
+// SSID to use in provisioning mode
+//  - you must connect to this to configure the WiFi settings for your access point
+const char provisionSsid[] = "wifi101-provisioning"; 
+// Hostname to use for the provision mode
+//  - once connected to the SSID above, you need to visit http://wifi101.io to configure WiFi settings
+const char provisionHost[] = "wifi101.io";
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+
+  // attempt to connect with previously provisioned settings
+  Serial.println("Attempting to connect with provisioned settings");
+  if (WiFi.begin() == WL_CONNECTED) {
+    Serial.println("Successfuly connected with provisioned settings");
+  } else {
+    Serial.println("Failed to connect with provisioned settings!");
+    Serial.println();
+    Serial.println("Entering provisioning mode");
+
+    if (WiFi.beginProvision(provisionSsid, provisionHost) != WL_PROVISIONING) {
+      Serial.println("Entering provisioning mode failed!");
+      while (1); // do nothing
+    }
+
+    Serial.print("Please connect to SSID '");
+    Serial.print(provisionSsid);
+    Serial.print("' and then visit http://");
+    Serial.print(provisionHost);
+    Serial.println(" to configure WiFi settings");
+
+    // wait to exit provisioning mode
+    while (WiFi.status() == WL_PROVISIONING);
+
+    if (WiFi.status() != WL_CONNECTED) {
+      // provisioning failed
+      Serial.println("Provisioning failed!");
+      while (1); // do nothing
+    }
+
+    Serial.println("WiFi provisioned successfully!");
+  }
+
+  server.begin();
+  // you're connected now, so print out the status:
+  printWifiStatus();
+}
+
+
+void loop() {
+  // listen for incoming clients
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // output the value of each analog input pin
+          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+            int sensorReading = analogRead(analogChannel);
+            client.print("analog input ");
+            client.print(analogChannel);
+            client.print(" is ");
+            client.print(sensorReading);
+            client.println("<br />");
+          }
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
+  }
+}
+
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
--- src/WiFi.cpp
+++ src/WiFi.cpp
@@ -47,6 +47,16 @@ extern "C" {
 static void wifi_cb(uint8_t u8MsgType, void *pvMsg)
 {
 	switch (u8MsgType) {
+		case M2M_WIFI_RESP_DEFAULT_CONNECT:
+		{
+			tstrM2MDefaultConnResp *pstrDefaultConnResp = (tstrM2MDefaultConnResp *)pvMsg;
+			if (pstrDefaultConnResp->s8ErrorCode) {
+				Serial.println(pstrDefaultConnResp->s8ErrorCode);
+				WiFi._status = WL_DISCONNECTED;
+			}
+		}
+		break;
+
 		case M2M_WIFI_RESP_CON_STATE_CHANGED:
 		{
 			tstrM2mWifiStateChanged *pstrWifiState = (tstrM2mWifiStateChanged *)pvMsg;
@@ -133,7 +143,7 @@ static void wifi_cb(uint8_t u8MsgType, void *pvMsg)
 				m2m_wifi_connect((char *)pstrProvInfo->au8SSID, strlen((char *)pstrProvInfo->au8SSID),
 						pstrProvInfo->u8SecType, pstrProvInfo->au8Password, M2M_WIFI_CH_ALL);
 			} else {
-				WiFi._status = WL_CONNECT_FAILED;
+				WiFi._status = WL_PROVISIONING_FAILED;
 				//SERIAL_PORT_MONITOR.println("wifi_cb: Provision failed.\r\n");
 			}
 		}
@@ -173,6 +183,8 @@ static void wifi_cb(uint8_t u8MsgType, void *pvMsg)
 				}
 				WiFi._remoteMacAddress = 0;
 			}
+
+			strcpy((char *)WiFi._ssid, pstrConnInfo->acSSID);
 		}
 		break;
 
@@ -326,11 +338,17 @@ uint8_t WiFiClass::begin()
 			millis() - start < 60000) {
 		m2m_wifi_handle_events(NULL);
 	}
-	if (!(_status & WL_CONNECTED)) {
-		_mode = WL_RESET_MODE;
-	}
 
 	memset(_ssid, 0, M2M_MAX_SSID_LEN);
+
+	if (!(_status & WL_CONNECTED)) {
+		_mode = WL_RESET_MODE;
+	} else {
+		m2m_wifi_get_connection_info();
+
+		m2m_wifi_handle_events(NULL);
+	}
+
 	return _status;
 }
 
@@ -480,12 +498,12 @@ uint8_t WiFiClass::startAP(const char *ssid, uint8_t u8SecType, const void *pvAu
 	return _status;
 }
 
-uint8_t WiFiClass::beginProvision(char *ssid, char *url)
+uint8_t WiFiClass::beginProvision(const char *ssid, const char *url)
 {
 	return beginProvision(ssid, url, 1);
 }
 
-uint8_t WiFiClass::beginProvision(char *ssid, char *url, uint8_t channel)
+uint8_t WiFiClass::beginProvision(const char *ssid, const char *url, uint8_t channel)
 {
 	tstrM2MAPConfig strM2MAPConfig;
 
@@ -499,16 +517,16 @@ uint8_t WiFiClass::beginProvision(char *ssid, char *url, uint8_t channel)
 	strM2MAPConfig.u8ListenChannel = channel;
 	strM2MAPConfig.u8SecType = M2M_WIFI_SEC_OPEN;
 	strM2MAPConfig.u8SsidHide = SSID_MODE_VISIBLE;
-	strM2MAPConfig.au8DHCPServerIP[0] = 0xC0; /* 192 */
-	strM2MAPConfig.au8DHCPServerIP[1] = 0xA8; /* 168 */
-	strM2MAPConfig.au8DHCPServerIP[2] = 0x01; /* 1 */
-	strM2MAPConfig.au8DHCPServerIP[3] = 0x01; /* 1 */
+	strM2MAPConfig.au8DHCPServerIP[0] = 192;
+	strM2MAPConfig.au8DHCPServerIP[1] = 168;
+	strM2MAPConfig.au8DHCPServerIP[2] = 1;
+	strM2MAPConfig.au8DHCPServerIP[3] = 1;
 
-	if (m2m_wifi_start_provision_mode((tstrM2MAPConfig *)&strM2MAPConfig, url, 1) < 0) {
-		_status = WL_CONNECT_FAILED;
+	if (m2m_wifi_start_provision_mode((tstrM2MAPConfig *)&strM2MAPConfig, (char*)url, 1) < 0) {
+		_status = WL_PROVISIONING_FAILED;
 		return _status;
 	}
-	_status = WL_CONNECTED;
+	_status = WL_PROVISIONING;
 	_mode = WL_PROV_MODE;
 
 	memset(_ssid, 0, M2M_MAX_SSID_LEN);
@@ -526,6 +544,8 @@ uint8_t WiFiClass::beginProvision(char *ssid, char *url, uint8_t channel)
 
 uint32_t WiFiClass::provisioned()
 {
+	m2m_wifi_handle_events(NULL);
+
 	if (_mode == WL_STA_MODE) {
 		return 1;
 	}
